a4a34a9fd5231dc8e23dee43847e6a2f91056f0e
dadenusto2 <57599004+dadenusto2@users.noreply.github.com>
2019-12-01 18:08:11 +0300

    Update my1.js

--- my1.js
+++ my1.js
@@ -1,25 +1,31 @@
-
-$( document ).ready(function() {
-    $("#btn").click(
-		function(){
-			sendAjaxForm('result_form', 'ajax_form', 'https://formcarry.com/s/hCN-DAf_L1A');
-			return false; 
-		}
-	);
-});
- 
-function sendAjaxForm(result_form, ajax_form, url) {
-    $.ajax({
-        url:     url,
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
-        	result = $.parseJSON(response);
-        	$('#result_form').html('Name: '+result.Name+'<br>E-mail: '+result.email+'<br>Comment: '+result.Comment);
-    	},
-    	error: function(response) { // Данные не отправлены
-            $('#result_form').html('Ошибка. Данные не отправлены.');
-    	}
- 	});
-}
+function sendAjaxForm(result_form, ajax_form) {
+    s="";
+    let n = document.forms["form"]["Name"].value;
+    if (n == "") {
+      s+="Input name\n";
+    }
+    let e = document.forms["form"]["E-mail"].value;
+    if (e == "") {
+        s+="Input e-mail\n";
+    }
+    let c = document.forms["form"]["Comment"].value;
+    if (c == "") {
+        s+="Input comment\n";
+    }
+    if(s!=""){
+        alert(s);
+        return 0;
+    }
+    else{
+        $.ajax({
+            url:     "https://formcarry.com/s/hCN-DAf_L1A",
+            type:     "POST", //метод отправки
+            dataType: "html", //формат данных
+            data: $(ajax_form).serialize()
+        });    
+        document.forms["form"]["Name"].value="";
+        document.forms["form"]["E-mail"].value="";
+        document.forms["form"]["Comment"].value="";
+        alert("Thanks for your comment!");
+        } 
+    }
