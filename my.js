fcfcada4219389bb6b9b946cfaf6e7752766cd7c
dadenusto2 <57599004+dadenusto2@users.noreply.github.com>
2019-12-01 15:53:34 +0300

    Update my.js

--- my.js
+++ my.js
@@ -23,30 +23,3 @@ function openbox(box){
     }else{
           document.getElementById(id).style.display='none';
     }
-}
-/* Article FructCode.com */
-$( document ).ready(function() {
-    $("#btn").click(
-		function(){
-			sendAjaxForm('result_form', 'ajax_form', 'action_ajax_form.php');
-			return false; 
-		}
-	);
-});
- 
-function sendAjaxForm(result_form, ajax_form, url) {
-    $.ajax({
-        url:     "https://formcarry.com/s/P7UpH6DEyGf",
-        type:     "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#"+ajax_form).serialize(),  // Сеарилизуем объект
-        success: function(response) { //Данные отправлены успешно
-        	result = $.parseJSON(response);
-        	$('#result_form').html('Name: '+result.Name+'<br>E-mail: '+result.email+'<br>Comment: '+result.Comment);
-    	},
-    	error: function(response) { // Данные не отправлены
-            $('#result_form').html('Ошибка. Данные не отправлены.');
-    	}
- 	});
-}
-  
